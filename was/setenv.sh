63d9758a69e02cbb970c64d4d80a37926a4fc385
Emanuele Disco <emawind84@gmail.com>
2017-02-22 16:35:25 +0900

    bug fix

--- was/setenv.sh
+++ was/setenv.sh
@@ -7,10 +7,10 @@
 
 CATALINA_OPTS="${CATALINA_OPTS} -server \
 -Dfile.encoding=UTF-8 \
--Xms256m -Xmx\${JAVA_MAX_SIZE:-512m} -XX:PermSize=64m -XX:MaxPermSize=512m \
+-Xms256m -Xmx${JAVA_MAX_SIZE:-512m} -XX:PermSize=64m -XX:MaxPermSize=512m \
 -Djava.awt.headless=true \
 -Djava.security.egd=file:/dev/./urandom \
--Xloggc:\${CATALINA_HOME}/logs/gc.log -verbose:gc \
+-Xloggc:${CATALINA_HOME}/logs/gc.log -verbose:gc \
 -XX:+PrintGCDateStamps -XX:+PrintGCDetails \
 -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
 
@@ -22,8 +22,8 @@ if [ -n "$JMX_PORT" ]; then
     CATALINA_OPTS="$CATALINA_OPTS \
     -Dcom.sun.management.jmxremote=true \
     -Dcom.sun.management.jmxremote.local.only=false \
-    -Dcom.sun.management.jmxremote.port=\${JMX_PORT} \
-    -Dcom.sun.management.jmxremote.rmi.port=\${JMX_PORT} \
+    -Dcom.sun.management.jmxremote.port=${JMX_PORT} \
+    -Dcom.sun.management.jmxremote.rmi.port=${JMX_PORT} \
     -Dcom.sun.management.jmxremote.ssl=false \
     -Dcom.sun.management.jmxremote.authenticate=false \
     -Djava.rmi.server.hostname=${JMX_HOST}"
